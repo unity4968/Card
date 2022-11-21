1f3048e15d568714954bd0d7b87c7782374dcd83
unity <unity4968@gmail.com>
2022-11-21 12:33:47 +0530

    create server on node

--- Assets/Scripts/TestObject.cs
+++ Assets/Scripts/TestObject.cs
@@ -3,24 +3,29 @@ using System.Collections.Generic;
 using Socket.Quobject.SocketIoClientDotNet.Client;
 using UnityEngine;
 
-public class TestObject : MonoBehaviour {
-  private QSocket socket;
+public class TestObject : MonoBehaviour
+{
+    private QSocket socket;
 
-  void Start () {
-    Debug.Log ("start");
-    socket = IO.Socket ("http://localhost:3000");
+    void Start()
+    {
+        Debug.Log("start");
+        socket = IO.Socket("http://localhost:3000");
 
-    socket.On (QSocket.EVENT_CONNECT, () => {
-      Debug.Log ("Connected");
-      socket.Emit ("chat", "test");
-    });
+        socket.On(QSocket.EVENT_CONNECT, () =>
+        {
+            Debug.Log("Connected");
+            socket.Emit("chat", "test");
+        });
 
-    socket.On ("chat", data => {
-      Debug.Log ("data : " + data);
-    });
-  }
+        socket.On("chat", data =>
+        {
+            Debug.Log("data : " + data);
+        });
+    }
 
-  private void OnDestroy () {
-    socket.Disconnect ();
-  }
+    private void OnDestroy()
+    {
+        socket.Disconnect();
+    }
 }
